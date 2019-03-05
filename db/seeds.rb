d0dffdf3d3792057ef885b3ad891e06dfbca3cbb
Svitlana Piddubna <sv.piddubna@gmail.com>
2019-03-05 20:11:28 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-u1 = User.create!(email: "ann@example.com", password: '111111')
-u2 = User.create!(email: "david@example.com", password: '111111')
-
 Task.destroy_all
+Event.destroy_all
+Question.destroy_all
+User.destroy_all
+u1 = User.create!(email: "ann@example.com", password: '111111', address: "Rudi-Dutschke-Straße 26, 10969 Berlin", first_name: "Ann", last_name: "Evans")
+u2 = User.create!(email: "david@example.com", password: '111111', address: "Markgrafenstraße 58, 10117 Berlin", first_name: "David", last_name: "Jones")
+
+
 Task.create!(title: "Heavy fridge", content: 'Need help', done: true, user: u1)
 Task.create!(title: "Need some greenery in my apt", content: 'Will adopt your dying plants', done: false, user: u2)
 
-Event.destroy_all
+
 Event.create!(title: "BBQ in Treptower Park", content: "BYOB!", address: "Checkpoint Charlie", starts_at: Time.zone.now.to_datetime , user: u2)
 
 Event.create!(title: "Yoga in Bellevue Park", content: "Sun salutations for all levels", address: 'Treptower Park', starts_at: Time.zone.now.to_datetime , user: u2)
 
-Question.destroy_all
+
 Question.create!(title: "Supermarket", content: "What's the best discounter here?", user: u1)
 
 Question.create!(title: "New events", content: "When is the next gathering?", user: u2)
