d20c50ecc83048df761643c73b341099355eb9c9
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-03-06 17:41:47 +0100

    added photo upload to events and tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,6 +16,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :photo)
   end
 end
