6f315bef678a8333203d2e3ce9c58d1e0f1877cf
Roger Dong <r.dong90@gmail.com>
2019-03-07 15:11:49 +0100

    refactor task controller code for set_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,15 @@
 class TasksController < ApplicationController
+  before_action :set_task, only [:update, :show]
 
   def index
     @tasks = Task.all
   end
 
+  def update
+  end
+
 
   def show
-    @task = Task.find(params[:id])
     @comment = Comment.new
     @post = @task.becomes(Post)
   end
@@ -22,6 +25,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :content, :photo)
