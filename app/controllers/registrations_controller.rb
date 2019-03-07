3e4579fffefbcd879007fc9bfd906bc0e9d2e641
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-03-07 12:02:01 +0100

    changed root path when logged in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/feed' # Or :prefix_to_your_route
+    feeds_path # Or :prefix_to_your_route
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
+  authenticated :user do
+    root to: 'feeds#index'
+  end
+
   root to: 'pages#home'
   resources :feeds, path: :feed, only: [:index, :show]
   resources :tasks, only: [:new, :create, :show]
