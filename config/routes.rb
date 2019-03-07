6b8facd8e75d9dbe4e3d6fbedbd3b89558c34580
Roger Dong <r.dong90@gmail.com>
2019-03-07 11:15:53 +0100

    added index routes for questions, events and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :feeds, path: :feed, only: [:index]
-  resources :tasks, only: [:new, :create, :show]
-  resources :questions, only: [:new, :create, :show]
-  resources :events, only: [:new, :create, :show]
+  resources :tasks, only: [:new, :create, :show, :index]
+  resources :questions, only: [:new, :create, :show, :index]
+  resources :events, only: [:new, :create, :show, :index]
 end
