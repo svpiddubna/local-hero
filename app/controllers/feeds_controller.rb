9c82cf464cf6e1de44382fc47e487e4b8b491a26
Roger Dong <r.dong90@gmail.com>
2019-03-07 16:34:58 +0100

    create user markers by lat/long

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -4,5 +4,23 @@ class FeedsController < ApplicationController
     @events = Event.all
     @questions = Question.all
     @tasks = Task.all
+
+    @user = User.where.not(latitude: nil, longitude: nil)
+    @marker = @users.map do |user|
+      {
+        lng: flat.longitude,
+        lat: flat.latitude
+      }
+
   end
 end
+
+
+
+    @flats = Flat.where.not(latitude: nil, longitude: nil)
+
+    @markers = @flats.map do |flat|
+      {
+        lng: flat.longitude,
+        lat: flat.latitude
+      }
