8e53dcc0725d59e38a3e45011207ffe1a4f0013d
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-03-07 11:42:33 +0100

    changed redirect after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
   resources :feeds, path: :feed, only: [:index, :show]
   resources :tasks, only: [:new, :create, :show]
