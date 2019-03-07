d280076c5184c063545601e5a4fa344a76004ae3
Roger Dong <r.dong90@gmail.com>
2019-03-07 15:05:00 +0100

    create default value false for boolean done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_160158) do
+ActiveRecord::Schema.define(version: 2019_03_07_135957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_03_06_160158) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.boolean "done"
+    t.boolean "done", default: false
     t.datetime "starts_at"
     t.string "address"
     t.string "type"
