13af3ce1714c5154efe3887039f2c15d6cdb5184
Thomas Etscheidt <etscheidtt@yahoo.com>
2019-03-11 15:36:23 +0100

    added circle radius

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,16 +14,50 @@ const buildMap = () => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .addTo(map);
+    .setLngLat([ marker.lng, marker.lat ])
+    .addTo(map);
   });
 };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 12, duration: 0 });
 };
+
+const drawCircleRadius = (map, marker) => {
+  map.on('style.load', function() {
+    map.addLayer({
+      "id": "alert_radius",
+      "type": "circle",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "FeatureCollection",
+          "features": [{
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [marker.lng, marker.lat]
+            }
+          }]
+        }
+      },
+      "paint": {
+        "circle-radius": {
+          stops: [
+            [0, 0],
+            [15, 1000]
+          ],
+          base: 2
+        },
+        "circle-color": "#fdb631",
+        "circle-opacity": 0.2
+      }
+    });
+  });
+}
+
 
 const initMapbox = () => {
   if (mapElement) {
@@ -31,7 +65,9 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    drawCircleRadius(map, markers[0]);
   }
 };
+
 
 export { initMapbox };
