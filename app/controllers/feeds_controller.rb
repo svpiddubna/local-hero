7eeca86372239de187a9f359d099aaaea44753f4
Roger Dong <r.dong90@gmail.com>
2019-03-11 11:22:16 +0100

    added methods to call top and recent users

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,9 +1,16 @@
 class FeedsController < ApplicationController
   def index
+
+    # Core methods
+
     @posts = Post.all.order(created_at: :desc)
     @events = Event.all.order(created_at: :desc)
     @questions = Question.all.order(created_at: :desc)
     @tasks = Task.all.order(created_at: :desc)
+
+    # Methods for retrieving top and most recent localheroes
+    @top_localheroes = User.order('votes DESC').limit(3)
+    @recent_localheroes = User.order('created_at DESC').limit(3)
 
     @users = User.where.not(latitude: nil, longitude: nil)
     # @markers = @users.map do |user|
